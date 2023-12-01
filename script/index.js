238c409f20d9081cc609426547360d8cbb332734
João Pedro <83091138+JoaoMartins160@users.noreply.github.com>
2023-12-01 19:15:45 -0300

    feature/Localstorage/displayelements

--- script/index.js
+++ script/index.js
@@ -23,19 +23,48 @@ function addMensage() {
   localStorage.setItem("dataList", JSON.stringify(dataList));
 }
 
-function deleteItens(dataList){
-  dataList.splice(0,1);
-  localStorage.removeItem(dataList.key);
-}
+function deleteItens(id) {
+  const index = dataList.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    dataList.splice(index, 1);
+    localStorage.setItem("dataList", JSON.stringify(dataList));
 
-function deleteInfo(){
-  var elements = document.querySelectorAll("input");
-  for(const formElemens of elements){
-    elements.value = '';
+    displayData();
   }
 }
 
-function deleteAll(dataList){
+function deleteAll() {
   dataList.splice(0, dataList.length);
   localStorage.clear();
+  displayData();
 }
+
+function displayData() {
+  const dataTable = document.getElementById("tabela");
+  const tbody = dataTable.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  dataList.forEach((data) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${data.nome}</td>
+      <td>${data.cpf}</td>
+      <td>${data.Telefone}</td>
+      <td>${data.service}</td>
+      <td>${data.DataEnvio}</td>
+      <td><button class="buttondel" onclick="deleteItens(${data.id})"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+  </svg></button></td>
+    `;
+    tbody.appendChild(row);
+  });
+}
+
+function searchByName(name) {
+  const filteredData = dataList.filter((data) =>
+    data.nome.toLowerCase().includes(name.toLowerCase())
+  );
+  displayData(filteredData);
+}
+
+displayData();
